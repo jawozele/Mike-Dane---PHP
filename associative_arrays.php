75357a8a4b704ab012bee1b8191a5a07d3e67fd7
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-09-28 17:49:27 +0100

    Update associative_arrays.php

--- associative_arrays.php
+++ associative_arrays.php
@@ -6,24 +6,20 @@
 <body>
 
 <form action="associative_arrays.php" method="post">
-	<input type="text" name="student">
-	<input type="submit">
+    <input type="text" name="student">
+    <input type="submit">
 </form>
 
 
 
 <?php
-
-	$grades = array("Jim"=>"A+", "Josh"=>"B-", "Pam"=>"C+", "Oscar"=>"D+");
-	echo $grades[$_POST["student"]];
-
-
-
-
-
-
-
-
+$grades = array(
+    "Jim" => "A+",
+    "Josh" => "B-",
+    "Pam" => "C+",
+    "Oscar" => "D+"
+);
+echo $grades[$_POST["student"]];
 ?>
 </body>
 </html>
