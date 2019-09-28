871ca02c05f5e7d2f2d64c572ce372b8a1acab0c
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-09-28 17:52:14 +0100

    Update checkboxes.php

--- checkboxes.php
+++ checkboxes.php
@@ -5,26 +5,17 @@
 </head>
 <body>
 
-	<form action="checkboxes.php" method="post">
-		Apples: <input type="checkbox" name="fruits[]" value="apples"><br>
-		Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>
-		Pears: <input type="checkbox" name="fruits[]" value="pears"><br>
-		<input type="submit">
+    <form action="checkboxes.php" method="post">
+        Apples: <input type="checkbox" name="fruits[]" value="apples"><br>
+        Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>
+        Pears: <input type="checkbox" name="fruits[]" value="pears"><br>
+        <input type="submit">
 
 
 
-	</form>
+    </form>
 
 
-	<?php
-
-		$fruits = $_POST["fruits"];
-		echo $fruits[0];
-
-
-
-	?>
-
-
-</body>
-</html>
+    <?php
+$fruits = $_POST["fruits"];
+echo $fruits[0];
