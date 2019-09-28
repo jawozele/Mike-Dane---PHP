b5f76a08f5218f29b0a0280cf817dc700575f6a9
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-09-28 17:54:24 +0100

    Update variables.php

--- variables.php
+++ variables.php
@@ -7,17 +7,17 @@
 
 
 <?php
-    $characterName = "John";
-    $characterAge = 35;
+$characterName = "John";
+$characterAge  = 35;
 
-    echo "<h1>Random Man</h1>";
-    echo "There once was a man named $characterName <br>";
-    echo "He was $characterAge <br>";
-    echo "He really liked the name $characterName";
-    echo "But didn't like being $characterAge <br>";
-    
-    
-    ?>
+echo "<h1>Random Man</h1>";
+echo "There once was a man named $characterName <br>";
+echo "He was $characterAge <br>";
+echo "He really liked the name $characterName";
+echo "But didn't like being $characterAge <br>";
+
+
+?>
 
 </body>
 </html>
