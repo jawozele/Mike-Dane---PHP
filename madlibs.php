e8b74066011bd39e75e7e08447853dfe850dea38
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-09-28 18:06:18 +0100

    Update madlibs.php

--- madlibs.php
+++ madlibs.php
@@ -6,22 +6,23 @@
 <body>
 
 <form action="madlibs.php" method="get">
-	Color: <input type="text" name="color"> <br>
-	Plural Noun: <input type="text" name="pluralNoun">
-	Celebrity: <input type="text" name="celebrity"> <br>
-	<input type="submit">
+    Color: <input type="text" name="color"> <br>
+    Plural Noun: <input type="text" name="pluralNoun">
+    Celebrity: <input type="text" name="celebrity"> <br>
+    <input type="submit">
 </form>
 <br><br>
 
 
 <?php
-	$color = $_GET["color"];
-	$pluralNoun = $_GET["pluralNoun"];
-	$celebrity = $_GET["celebrity"];
+$color      = $_GET["color"];
+$pluralNoun = $_GET["pluralNoun"];
+$celebrity  = $_GET["celebrity"];
 
-	echo "Roses are $color <br>";
-	echo "$pluralNoun is blue <br>";
-	echo "I love $celebrity <br>";
+echo "Roses are $color <br>";
+echo "$pluralNoun is blue <br>";
+echo "I love $celebrity <br>";
 ?>
 </body>
 </html>
+Download
