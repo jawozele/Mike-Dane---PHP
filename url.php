71343bb85142769e1e4ec68d59360fd95d7c4c3a
Josh Awozele <35323325+jawozele@users.noreply.github.com>
2019-09-28 17:55:14 +0100

    Update url.php

--- url.php
+++ url.php
@@ -7,14 +7,14 @@
 
 
 <form action="url.php" method="get">
-	Name: <input type="text" name="name" <br>
-	<input type="submit">
+    Name: <input type="text" name="name" <br>
+    <input type="submit">
 </form>
 <br><br>
 
 
 <?php
-	echo $_GET["name"];
+echo $_GET["name"];
 ?>
 
 </body>
